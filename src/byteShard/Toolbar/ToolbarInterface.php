9985d46a8aa5b8506ac218ef476d2f07162a0945
Lars Hennig <lars@bespin-studios.com>
2026-01-03 15:18:12 +0100

    feat: refactor ToolbarInterface and create new ToolbarClassInterface

--- src/byteShard/Toolbar/ToolbarInterface.php
+++ src/byteShard/Toolbar/ToolbarInterface.php
@@ -2,20 +2,7 @@
 
 namespace byteShard\Toolbar;
 
-
-use byteShard\Enum\Access;
-use byteShard\Internal\Struct\UiComponentInterface;
-
 interface ToolbarInterface
 {
-    public function addToolbarObject(ToolbarObjectInterface ...$toolbarObjects): self;
-
-    /**
-     * @return array<string, string | array<mixed>>
-     */
-    public function getContents(): array;
-
-    public function getComponent(): ?UiComponentInterface;
-
-    public function setAccessType(int|Access $accessType): self;
+    public function defineToolbarContent(): void;
 }
